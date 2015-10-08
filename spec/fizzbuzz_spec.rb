6ce2ca23cc01a4f1b5d6e15b0cfc31183553e728
Michael Roger <bodgeuk12@gmail.com>
2015-10-08 15:23:56 +0100

    Expected buzz when divisible by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,14 @@ require_relative '../lib/fizzbuzz.rb'
 
 describe 'Fizzbuzz' do
   it 'returns Fizz when given 3' do
-    expect(Fizzbuzz(3)).to eq 'Fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
+
+  it 'returns Buzz when divisible by 5' do
+    expect(fizzbuzz(20)).to eq 'Buzz'
+  end
+
 end
+
+
 
